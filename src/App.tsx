63fb063ba0668abd35c6bfe84f9f66143f0da10e
Sergei Bozhko <sbozhko@mpi-sws.org>
2024-03-16 21:22:12 +0100

    split a few components into smaller chunks

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,6 @@ function getRandomFaceId() {
     .padStart(5, "0");
 }
 
-const Face: React.FC<{ faceId: string }> = ({ faceId }) => {
-  const url = `https://whichfaceisreal.blob.core.windows.net/public/realimages/${faceId}.jpeg`;
-  return <img src={url} className="rounded-full h-60 w-60" />;
-};
-
 const HotKey: React.FC<{ value: string }> = ({ value }) => {
   return (
     <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
@@ -78,7 +73,7 @@ function App() {
     getNextFace();
   };
 
-  const Start = () => {
+  const Start: React.FC = () => {
     useEffect(() => {
       const handleKeyDown = (e: KeyboardEvent) => {
         if (e.key == " " || e.code == "Space") {
@@ -90,11 +85,11 @@ function App() {
       return () => document.removeEventListener("keydown", handleKeyDown);
     }, []);
 
-    const Title = () => {
+    const Title: React.FC = () => {
       return <h1 className="text-3xl text-center m-3 p-3 border-b text-blue-900">Human Face Retention Benchmark</h1>;
     };
 
-    const StartButton = () => {
+    const StartButton: React.FC = () => {
       return (
         <Button
           className="m-1"
@@ -110,7 +105,7 @@ function App() {
       );
     };
 
-    const Instructions = () => {
+    const Instructions: React.FC = () => {
       const instructions =
         'You will be presented with a series of faces. Press "new" if the face is being displayed for the first time. Press "seen" if the face has been shown previously. Get as many right answers as possible. You are allowed to make up to three mistakes.';
 
@@ -137,7 +132,7 @@ function App() {
     );
   };
 
-  const Game = () => {
+  const Game: React.FC = () => {
     useEffect(() => {
       const handleKeyDown = (e: KeyboardEvent) => {
         if (e.key === "ArrowLeft") {
@@ -152,15 +147,22 @@ function App() {
       return () => document.removeEventListener("keydown", handleKeyDown);
     }, []);
 
-    return (
-      <div className="p-6 bg-slate-50 h-full ">
+    const Score: React.FC = () => {
+      return (
         <div className="flex justify-evenly text-2xl border-b">
           <p className=" text-2xl">Lives {lives}</p>
           <p>Score {score}</p>
         </div>
-        <div className="flex justify-center pt-4">
-          <Face faceId={currentFace!} />
-        </div>
+      );
+    };
+
+    const Face: React.FC<{ faceId: string }> = ({ faceId }) => {
+      const url = `https://whichfaceisreal.blob.core.windows.net/public/realimages/${faceId}.jpeg`;
+      return <img src={url} className="rounded-full h-60 w-60" />;
+    };
+
+    const Controls: React.FC = () => {
+      return (
         <div className="flex justify-center">
           <Button className="my-4 mx-2" variant="outline" size="default" onClick={seenClick}>
             <p>seen &nbsp;</p> <HotKey value={"←"} />
@@ -169,11 +171,21 @@ function App() {
             <p>new &nbsp;</p> <HotKey value={"→"} />
           </Button>
         </div>
+      );
+    };
+
+    return (
+      <div className="p-6 bg-slate-50 h-full ">
+        <Score />
+        <div className="flex justify-center pt-4">
+          <Face faceId={currentFace!} />
+        </div>
+        <Controls />
       </div>
     );
   };
 
-  const FinalScore = () => {
+  const FinalScore: React.FC = () => {
     useEffect(() => {
       const handleKeyDown = (e: KeyboardEvent) => {
         if (e.key == " " || e.code == "Space") {
@@ -185,31 +197,37 @@ function App() {
       return () => document.removeEventListener("keydown", handleKeyDown);
     }, []);
 
+    const Score: React.FC = () => {
+      return <p className="w-full text-center">Your score: {score}</p>;
+    };
+
+    const PlayAgainButton: React.FC = () => {
+      return (
+        <Button
+          className="text-black m-4"
+          variant="outline"
+          size="lg"
+          onClick={() => {
+            window.location.reload();
+          }}
+        >
+          <p>Play again</p> &nbsp; <HotKey value={"⎵"} />
+        </Button>
+      );
+    };
+
     return (
       <div className="h-full flex flex-col justify-center items-center text-3xl text-blue-700 bg-slate-50">
         <div className="flex flex-col items-center justify-center">
-          <p className="w-full text-center">Your score: {score}</p>
-
-          <Button
-            className="text-black m-4"
-            variant="outline"
-            size="lg"
-            onClick={() => {
-              window.location.reload();
-            }}
-          >
-            <p>Play again</p> &nbsp; <HotKey value={"⎵"} />
-          </Button>
+          <Score />
+          <PlayAgainButton />
         </div>
       </div>
     );
   };
 
-  return (
-    <div className="flex justify-center pt-10 font-poppins">
-      <div className="w-96 h-96 flex-col items-center justify-center border-2">
-        {gameState == "start" ? <Start /> : gameState == "play" ? <Game /> : <FinalScore />}
-      </div>
+  const Cache: React.FC = () => {
+    return (
       <div>
         {/* For caching reasons, we load a few new images in advance. */}
         {faceQueue.map((e, i) => {
@@ -222,6 +240,14 @@ function App() {
           );
         })}
       </div>
+    );
+  };
+  return (
+    <div className="flex justify-center pt-10 font-poppins">
+      <div className="w-96 h-96 flex-col items-center justify-center border-2">
+        {gameState == "start" ? <Start /> : gameState == "play" ? <Game /> : <FinalScore />}
+      </div>
+      <Cache />
     </div>
   );
 }
